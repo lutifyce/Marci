50014a99fed39f49980444f0ac687f4116ee5ca5
lutifyce <lutifyce@mail.ru>
2022-11-23 18:37:58 +0300

    7.2

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,13 +1,19 @@
-var mongoose = require('mongoose')
+var mongoose= require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/test')
 
-var Ulti = mongoose.model('Ulti', { name: String })
+var schema= mongoose.Schema({ name: String})
 
-var spell = new Ulti({ name: 'Unleash'})
-spell.save(function (err) {
-if (err) {
-console.log(err)
-    } else {
-console.log('Arrrr')
-    }
+schema.methods.sidekick= function(){
+console.log(this.get("name") + " let out a cheerful whistle")
+}
+
+var Ulti= mongoose.model('Ulti', schema)
+
+var spell= new Ulti({ name: 'Mirana'})
+spell.save(function(err) {
+spell.sidekick()
 })
+
+
+
+

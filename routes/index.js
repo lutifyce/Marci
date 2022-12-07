e61d36098f15a9a2f5ffdd04fbfbe11fb02b5999
lutifyce <lutifyce@mail.ru>
2022-12-07 20:34:57 +0300

    8.5

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var marci = require("../models/marci").marci
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    marci.find({},{_id:0,title:1,nick:1},function(err,menu){
+        res.render('index', 
+        {
+          title: 'Express',
+          menu: menu
+        });
+    })
+
 });
 
 module.exports = router;
--- routes/marci.js
+++ routes/marci.js
@@ -1,24 +1,34 @@
 var express = require('express');
 var router = express.Router();
 var marci = require("../models/marci").marci
+var async = require("async")
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     res.send('Новый маршрутизатор, для маpшрутов, начинающихся с marci');
 });
-
-/* Страница Marci */
-router.get('/:nick', function (req, res, next) {
-  console.log('в роутере')
-  console.log(req.params.nick)
-    marci.findOne({ nick: req.params.nick }, function (err, marci) {
-        if (err) return next(err)
-        if (!marci) return next(new Error("Нет такой адаптации "))
-        res.render('Marci', {
-            title: marci.title,
-            picture: marci.avatar,
-            desc: marci.desc
-        })
-    })
+/*   */
+router.get('/:nick', function(req, res, next) {
+  async.parallel([
+          function(callback){
+              marci.findOne({nick:req.params.nick}, callback)
+          },
+          function(callback){
+              marci.find({},{_id:0,title:1,nick:1},callback)
+          }
+      ],
+      function(err,result){
+          if(err) return next(err)
+          var marci = result[0]
+          var marcis = result[1] || []
+          if(!marci) return next(new Error("Нет такой адаптации"))
+          res.render('marci', {
+              title: marci.title,
+              picture: marci.avatar,
+              desc: marci.desc,
+              menu: marcis
+          });
+      })
 })
-module.exports = router;
+
+module.exports = router

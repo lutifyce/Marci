e7b42114e5de9195f339ec272bdf18bfc950847a
lutifyce <lutifyce@mail.ru>
2023-01-16 03:58:28 +0300

    10.3

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,12 @@ router.get('/', function(req, res, next) {
 router.get('/logreg', function(req, res, next) {
   res.render('logreg',{title:'Вход'});
   });
+
+  /* POST login/registration page. */
+router.post('/logreg', function(req, res, next) {
+    var username = req.body.username
+    var password = req.body.password
+});
   
 
 module.exports = router;

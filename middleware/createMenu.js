c7e1e1913dcf1d7ee31af881ffb23c83ed49731f
lutifyce <lutifyce@mail.ru>
2023-01-16 07:03:27 +0300

    11.4

--- middleware/createMenu.js
+++ middleware/createMenu.js
@@ -1,11 +1,14 @@
-var Marci = require("../models/marci").Marci
+var Marci = require("./../models/marci").Marci
+
+
 
 module.exports = function(req,res,next){
-    res.locals.nav = []
+res.locals.nav = []
 
-    Marci.find(null,{_id:0,title:1,nick:1},function(err,result){
-        if(err) throw err
-        res.locals.nav = result
-        next()
-    })
+
+Marci.find(null,{_id:0,title:1,nick:1},function(err,result){
+if(err) throw err
+res.locals.nav = result
+next()
+})
 }

40092be18e1de043e72cfffa2730ba8e115f012a
lutifyce <lutifyce@mail.ru>
2023-01-16 04:02:07 +0300

    10.4

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var Marci = require("../models/marci").Marci
-
+var User = require("./../models/user").User
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -21,9 +21,26 @@ router.get('/logreg', function(req, res, next) {
 
   /* POST login/registration page. */
 router.post('/logreg', function(req, res, next) {
-    var username = req.body.username
-    var password = req.body.password
+  var username = req.body.username
+  var password = req.body.password
+User.findOne({username:username},function(err,user){
+    if(err) return next(err)
+    if(user){
+        if(user.checkPassword(password)){
+            req.session.user = user._id
+            res.redirect('/')
+        } else {
+                  res.render('logreg', {title: 'Вход'})
+        }
+    } else {
+        var user = new User({username:username,password:password})
+        user.save(function(err,user){
+            if(err) return next(err)
+            req.session.user = user._id
+            res.redirect('/')
+        })        
+  }
+})
 });
-  
 
 module.exports = router;
